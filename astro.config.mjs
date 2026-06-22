5d234fb9636b0b3c2bf47cbf45154a7c0635fc9c
NikosSpace25 <Nikostriantos25@outlook.com>
2026-06-22 09:12:29 +0300

    Button component, ενημερωση dependencies, Δημιουργια dynamicReveal και kitchens.astro
    μενει τελιξκκος ελεγχος για SEO, a11y κτλπ

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://kounouvelis.gr',
-  integrations: [sitemap()]
+  integrations: [sitemap()],
+  devToolbar: { enabled: false }
 });
